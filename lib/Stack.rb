0c5db5ae756079d709e21b5f6dc7afed07167391
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-02-26 12:27:45 -0800

    Wrote the Stack methods

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,31 @@ class Stack
   end
 
   def push(element)
+    # @store.unshift(element) #Anacelia
+    @store << element
   end
 
   def pop
+      return @store.pop
+    # alt
+    # last_element = @store.last
+    # @store.delete(@store.last)
+    # return last_element
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
+    # alt
+    # return @store.empty?
+    # return @store.length == 0
   end
 
   def to_s
